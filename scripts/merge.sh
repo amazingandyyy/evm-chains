e878fa9f34982c182f436fe67c87c8f640ed61e2
Andy Chen <amazingandyyy@gmail.com>
2021-11-11 11:53:38 +0000

    feat(scripts): merge data into ethereum-lists-chains.json

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -2,4 +2,10 @@
 
 set -ex
 
-echo "merging data..."
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+DOCSPATH=$SCRIPTPATH/../docs
+RAWDATAPATH=$SCRIPTPATH/../rawdata
+
+rm -rf $DOCSPATH/*
+
+jq -s 'flatten' $RAWDATAPATH/ethereum-lists/chains/*.json > $DOCSPATH/ethereum-lists-chains.json
